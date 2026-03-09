77d3ba5db7f2694b3760cc833062e5352749ebc5
NIRMAL V G <Nirmalvg97@gmail.com>
2026-03-09 16:23:02 +0530

    main.jsx updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App.jsx";
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    if (registrations.length > 1) {
-      registrations.slice(1).forEach((reg) => reg.unregister());
-    }
-  });
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
